c9e7a862567c55efb52ffb5725ecd7f404aa4e3c
MaddalenaRen <ileniarennella@gmail.com>
2025-03-21 18:04:48 +0100

    commit

--- script/homepage.js
+++ script/homepage.js
@@ -31,20 +31,28 @@ const getEvents = function () {
             row.innerHTML = "";
 
             data.forEach((wine) => {
+                const truncatedDescription = wine.description.length > 100 ? wine.description.substring(0, 100) + '...' : wine.description;
                 row.innerHTML += `
-                <div class="col col-12 col-lg-3 col-md-4 col-sm-6 my-3">
-                    <div class="card shadow-sm rounded-lg">
-                        <img src="${wine.imageUrl}" class="card-img-top" alt="${wine.name}" />
-                        <div class="card-body">
-                            <h5 class="card-title">${wine.name}</h5>
-                            <p class="card-text text-muted">${wine.description}</p>
-                            <p class="card-text font-weight-bold">${wine.price.toLocaleString()}€</p>
-                            <p class="card-text text-success">${wine.brand}</p>
-                            <a href="./details.html?id=${wine._id}" class="btn btn-primary btn-block">Vai ai dettagli</a>
-                        </div>
-                    </div>
+         <div class="col col-12 col-lg-3 col-md-4 col-sm-6 my-3">
+        <div class="card shadow-sm rounded-lg d-flex flex-column" style="height: 100%;">
+            <img src="${wine.imageUrl}" class="card-img-top" alt="${wine.name}" />
+            <div class="card-body d-flex flex-column flex-grow-1">
+                <h5 class="card-title">${wine.name}</h5>
+                <p class="card-text text-muted">${truncatedDescription}</p>
+                ${wine.description.length > 100 ? `<a href="#${wine._id}" class="btn btn-link"></a>` : ""}
+                
+                
+                <div class="d-flex justify-content-between align-items-center" style="min-height: 50px; display: flex;">
+                    <p class="card-text font-weight-bold" style="flex: 1; text-align: left; margin: 0;">${wine.price.toLocaleString()}€</p>
+                    <p class="card-text text-success" style="flex: 1; text-align: right; margin: 0;">${wine.brand}</p>
                 </div>
-                `;
+                
+                <a href="./details.html?id=${wine._id}" class="btn btn-primary mt-auto">Vai ai dettagli</a>
+            </div>
+        </div>
+    </div>
+    `;
+
             });
             document.getElementById("spinner-container").style.display = "none";
         })
